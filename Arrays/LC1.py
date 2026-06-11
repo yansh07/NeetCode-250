8e7c455701d13ef70cd1d2c7c76e24a56f1e6082
Priyanshu <pksingh69313@gmail.com>
2026-06-11 23:15:48 +0530

    Solved LC1

---
+++ Arrays/LC1.py
@@ -0,0 +1,15 @@
+#Two Sum
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+            
+            seen[num] = i
+        
+        return []
