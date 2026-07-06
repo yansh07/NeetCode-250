f78e746788c8ad3d2f63263b61637d439cd21553
Priyanshu <pksingh69313@gmail.com>
2026-07-06 19:06:00 +0530

    solved valid palindrome ii

---
+++ Two-Pointers/LC680.py
@@ -0,0 +1,18 @@
+# Vaid Palindrome II
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            
+            else:
+                skip_left = s[left + 1 : right + 1]
+                skip_right = s[left:right]
+
+                return skip_left == skip_left[::-1] or skip_right == skip_right[::-1]
+        
+        return True
