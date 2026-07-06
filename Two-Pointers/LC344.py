1e2e557f8ca6aa523f8dee33e5e26d524c983f12
Priyanshu <pksingh69313@gmail.com>
2026-07-06 17:29:49 +0530

    solved reverse string

--- Two-Pointers/LC344.py
+++ Two-Pointers/LC344.py
@@ -2,4 +2,9 @@
 
 class Solution:
     def reverseString(self, s: list[str]) -> None:
-        pass
+        
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
