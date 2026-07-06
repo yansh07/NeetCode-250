090326a15365ed9b6874f615b491c5b3d0b89e4d
Priyanshu <pksingh69313@gmail.com>
2026-07-06 19:45:30 +0530

    solved merge strings alternately

---
+++ Two-Pointers/LC1768.py
@@ -0,0 +1,16 @@
+# Merge Strings Alternatively
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = 0
+
+        while i < len(word1) and i < len(word2):
+            result.append(word1[i])
+            result.append(word2[i])
+            i += 1
+
+        result.append(word1[i:])
+        result.append(word2[i:])
+        
+        return "".join(result)
