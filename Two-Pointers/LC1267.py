0acf022e0a8bcb7d036fcddf85f1aa075dd408fa
Priyanshu <pksingh69313@gmail.com>
2026-07-11 17:39:52 +0530

    solve two sum ii - input array is sorted

---
+++ Two-Pointers/LC1267.py
@@ -0,0 +1,17 @@
+# Two Sum II - Input Array is Sorted
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum == target:
+                return [left+1, right+1]
+            
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
