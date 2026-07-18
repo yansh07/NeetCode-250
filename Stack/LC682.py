35983d698454bc8ee6aff77360a54a6a12cb1129
Priyanshu <pksingh69313@gmail.com>
2026-07-18 17:09:57 +0530

    add stack solutions, with baseball game

---
+++ Stack/LC682.py
@@ -0,0 +1,16 @@
+# Baseball game
+
+class Solution:
+    def calPoints(self, operations: list[str]) -> int:
+        stack = []
+
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "D":
+                stack.append(stack[-1] * 2)
+            elif op == "C":
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
